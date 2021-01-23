29d2f392cf16e694f22b3ade64d76e200b0e0af8
Rebekah Kahn <rebekah.m.kahn@gmail.com>
2021-01-23 13:01:22 -0500

    made changes to react navigation

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,71 +1,88 @@
-import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { createStackNavigator } from 'react-navigation-stack';
-import { createAppContainer } from 'react-navigation';
-import Mantises from './MantisesComponent';
-import MantisInfo from './MantisInfoComponent';
-import { Appbar } from 'react-native-paper';
+import React, { Component } from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { createStackNavigator } from "react-navigation-stack";
+import { createAppContainer } from "react-navigation";
+import Mantises from "./MantisesComponent";
+import MantisInfo from "./MantisInfoComponent";
+import { Appbar, IconButton } from "react-native-paper";
 
-const MantisesNavigator = createStackNavigator (
-    {
-        Mantises: { screen: Mantises },
-        MantisInfo: { screen: MantisInfo }
+const MantisesNavigator = createStackNavigator(
+  {
+    Mantises: {
+      screen: Mantises,
+      navigationOptions: ({ navigation }) => ({
+        headerLeft: (
+          <Appbar.Action
+            icon="format-align-justify"
+            onPress={() => navigation.toggleDrawer()}
+          />
+        ),
+
+        headerTitle: () => (
+          <View>
+            <Text style={styles.container}>Mantis Place</Text>
+          </View>
+        ),
+        headerRight: () => (
+          <View style={{ backgroundColor: "#59916d", flexDirection: "row" }}>
+            <Appbar.Action
+              icon="magnify"
+              onPress={() => console.log("Pressed magnify")}
+              style={styles.icon}
+            />
+            <Appbar.Action
+              icon="account"
+              onPress={() => console.log("Pressed account")}
+            />
+            <Appbar.Action
+              icon="cart"
+              onPress={() => console.log("Pressed cart")}
+            />
+          </View>
+        ),
+      }),
     },
-    {
-        initialRouteName: 'Mantises',
-        defaultNavigationOptions: {
-            headerStyle: {
-                backgroundColor: '#59916d',
-            },
-            headerTintColor: '#fff',
-            headerTitleStyle: {
-                color: '#fff'
-            },
-            headerTitle: () => (
-                <View>
-                    <Text
-                        style={styles.container}>Mantis Place
-                    </Text>
-                </View>
-            ),
-            headerLeft: () => (
-                <View>
-                    <Appbar.Action icon="format-align-justify"/>
-                </View>
-            ),
-            headerRight: () => (
-                <View style={{backgroundColor: '#59916d', flexDirection: 'row'}}>
-                    <Appbar.Action icon="magnify" style={styles.icon}/>
-                    <Appbar.Action icon="account" />
-                    <Appbar.Action icon="cart"  />
-                </View>
-            )
-        }
-    }
+
+    MantisInfo: { screen: MantisInfo },
+  },
+  {
+    initialRouteName: "Mantises",
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: "#59916d",
+      },
+      headerTintColor: "#fff",
+      headerTitleStyle: {
+        color: "#202020",
+        fontFamily: "Aladin",
+        fontSize: 30,
+      },
+    },
+  }
 );
 
 const AppNavigator = createAppContainer(MantisesNavigator);
 
 class Main extends Component {
-    render() {
-        return (
-            <View style={{flex: 1}}>
-                <AppNavigator />
-            </View>
-        );
-    }
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
+        <AppNavigator />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-      backgroundColor: '#59916d',
-      fontFamily: 'Aladin',
-      fontSize: 30,
-      color: '#202020',
-    },
-    icons: {
-        color:'white',
-    }
+  container: {
+    backgroundColor: "#59916d",
+    fontFamily: "Aladin",
+    fontSize: 30,
+    color: "#202020",
+  },
+  icons: {
+    color: "white",
+  },
 });
 
 export default Main;
--- components/MantisInfoComponent.js
+++ components/MantisInfoComponent.js
@@ -1,40 +1,38 @@
-import React, { Component } from 'react';
-import { View, Text } from 'react-native';
-import { Card } from 'react-native-paper';
-import { MANTISES } from '../shared/mantises';
+import React, { Component } from "react";
+import { View, Text } from "react-native";
+import { Card } from "react-native-paper";
+import { MANTISES } from "../shared/mantises";
 
-function RenderMantis({ mantis }){
-    if (mantis) {
-        return (
-            <Card
-                featuredTitle={mantis.name}
-            >
-                <Text style={{margin: 10}}>
-                    THIS IS BOGUS
-                </Text>
-            </Card>
-        );
-    }
-    return <View />;
+function RenderMantis({ mantis }) {
+  if (mantis) {
+    return (
+      <Card featuredTitle={mantis.name}>
+        <Text style={{ margin: 10 }}>{mantis.description}</Text>
+      </Card>
+    );
+  }
+  return <View />;
 }
 
 class MantisInfo extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            mantises: MANTISES
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      mantises: MANTISES,
+    };
+  }
 
-    static navigationOptions = {
-        title: 'Mantises Information'
-    }
+  static navigationOptions = {
+    title: "Mantis Info",
+  };
 
-    render () {
-        const mantisId = this.props.navigation.getParam('mantisId');
-        const mantis = this.state.mantises.filter(mantis => mantis.id === mantisId)[0];
-        return <RenderMantis mantis={mantis} />;
-    }
+  render() {
+    const mantisId = this.props.navigation.getParam("mantisId");
+    const mantis = this.state.mantises.filter(
+      (mantis) => mantis.id === mantisId
+    )[0];
+    return <RenderMantis mantis={mantis} />;
+  }
 }
 
 export default MantisInfo;
